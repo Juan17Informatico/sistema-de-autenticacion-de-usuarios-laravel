35929c9e01ce92c82908c5ce4f6cf7e25866717f
JuanCM <juancampuzano2356@gmail.com>
2025-03-03 15:55:21 -0500

    Se agrega plantilla para footer

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-gray-200 dark:bg-gray-900 text-center p-4 mt-6">
+    <p class="text-gray-600 dark:text-gray-400">© {{ date('Y') }} Mi Proyecto Laravel. Todos los derechos reservados.</p>
+</footer>
